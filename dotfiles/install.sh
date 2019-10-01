e81227e1928004208261cdf0d0ea2f5778a8c754
Joel E Carlson <joel.elmer.carlson@gmail.com>
2019-10-01 11:35:53 -0500

    1569947752

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -13,6 +13,9 @@ UBUNTU=$(uname -a|grep -i "Ubuntu"|wc -l)
 CENTOS=$(uname -a|grep -i "Centos"|wc -l)
 DEBIAN=$(uname -a|grep -i "Debian"|wc -l)
 
+# SPACE
+SPACE=$(df /home |awk '/\//{print $4}')
+
 # ~/.local/bin
 mkdir -p $HOME/.local/bin
 
@@ -20,8 +23,7 @@ mkdir -p $HOME/.local/bin
 DOTFILES=".bashrc .twmrc .xsession .Xdefaults .gitconfig .powerline-shell.json"
 for i in $DOTFILES
 do
-  echo $i
-  cp $i $HOME/
+    cp $i $HOME/
 done
 chmod a+x $HOME/.xsession
 
@@ -29,58 +31,61 @@ chmod a+x $HOME/.xsession
 XMONAD="$HOME/.xmonad"
 if [ ! -d $XMONAD ]
 then
-  mkdir $XMONAD
+    mkdir $XMONAD
 fi
 
 cp $HOME/stack/dotfiles/xmonad/xmonad.hs         $XMONAD
 cp $HOME/stack/dotfiles/xmonad/xmonad-session-rc $XMONAD
 
-# spacemacs
-EMACS="$HOME/.emacs.d"
-if [ ! -d $EMACS ]
+if [ $SPACE -gt 10000000 ]
 then
-  git clone https://github.com/syl20bnr/spacemacs $EMACS
+    # spacemacs
+    EMACS="$HOME/.emacs.d"
+    if [ ! -d $EMACS ]
+    then
+        git clone https://github.com/syl20bnr/spacemacs $EMACS
+    fi
+
+    # source code pro fonts
+    ADOBEFONT="/tmp/adobefont"
+    if [ -d $ADOBEFONT ]
+    then
+        rm -fr $ADOBEFONT
+    fi
+
+    if [ ! -d $ADOBEFONT ]
+    then
+        mkdir $ADOBEFONT
+    fi
+    cd $ADOBEFONT
+        cp $HOME/stack/adobefonts/1.017R.zip $ADOBEFONT
+        unzip 1.017R.zip
+
+    FONTS="$HOME/.fonts"
+    if [ ! -d $FONTS ]
+    then
+        mkdir $FONTS
+    fi
+    cp source-code-pro-1.017R/OTF/*.otf ~/.fonts/
+
+    # font cache
+    fc-cache -f -v
+
+    # powerline
+    cd $HOME
+    if [ ! -d powerline-fonts ]
+    then
+        git clone https://github.com/Lokaltog/powerline-fonts
+    fi
+    cd powerline-fonts
+    ./install.sh
 fi
 
-# source code pro fonts
-ADOBEFONT="/tmp/adobefont"
-if [ -d $ADOBEFONT ]
-then
-  rm -fr $ADOBEFONT
-fi
-
-if [ ! -d $ADOBEFONT ]
-then
-  mkdir $ADOBEFONT
-fi
-cd $ADOBEFONT
-cp $HOME/stack/adobefonts/1.017R.zip $ADOBEFONT
-unzip 1.017R.zip
-
-FONTS="$HOME/.fonts"
-if [ ! -d $FONTS ]
-then
-  mkdir $FONTS
-fi
-cp source-code-pro-1.017R/OTF/*.otf ~/.fonts/
-
-
-# font cache
-fc-cache -f -v
-
-# powerline
-cd $HOME
-if [ ! -d powerline-fonts ]
-then
-  git clone https://github.com/Lokaltog/powerline-fonts
-fi
-cd powerline-fonts
-./install.sh
 
 cd $HOME
 if [ ! -d powerline-shell ]
 then
-  git clone https://github.com/milkbikis/powerline-shell
+    git clone https://github.com/milkbikis/powerline-shell
 fi
 cd powerline-shell
 
