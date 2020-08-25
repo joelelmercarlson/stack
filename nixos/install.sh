6d665a3df06fe34d343a7471c7bef8070475b6f0
Joel E Carlson <joel.elmer.carlson@gmail.com>
2020-08-25 19:47:10 +0000

    nixos_install_sh

--- nixos/install.sh
+++ nixos/install.sh
@@ -10,6 +10,7 @@ set -ex
 
 cd $HOME
 
+echo "dotfiles..."
 # ~/.local/bin
 mkdir -p $HOME/.local/bin
 
@@ -19,9 +20,20 @@ for i in $XS; do
     cp $i $HOME/
 done
 
+echo "software..."
+
 # stack for nix
 mkdir $HOME/.stack
 cp config.yaml $HOME/.stack
+
+# haskell
+$HOME/stack/nixos/haskell.sh
+
+# python3
+sudo nix-env -iA nixos.python3
+
+# vim
+sudo nix-env -iA nixos.vim
 
 # starship.rs
 sudo nix-env -iA nixos.starship
