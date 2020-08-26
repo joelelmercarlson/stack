b0eab410d2461510c82c713e0e96d61e771c4441
Joel E Carlson <joel.elmer.carlson@gmail.com>
2020-08-26 15:50:13 -0500

    nixos

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -75,8 +75,8 @@
   # services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
