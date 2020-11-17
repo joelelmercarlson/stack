fe84ec7d5ac58bd5945362ada5cb7f88b0ba361d
Joel E Carlson <joel_carlson@optum.com>
2020-11-17 11:15:44 -0600

    mkisofs.sh

---
+++ fedora/mkisofs.sh
@@ -0,0 +1,6 @@
+mkisofs -o /export/rhel8.iso \
+-b isolinux/isolinux.bin -J -R -l \
+-c isolinux/boot.cat \
+-no-emul-boot -boot-load-size 4 -boot-info-table \
+-eltorito-alt-boot \
+-e images/efiboot.img -no-emul-boot -graft-points -V "RHEL" .
