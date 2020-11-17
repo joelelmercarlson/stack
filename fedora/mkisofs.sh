354d919deafe07983dfe5ef30aa82031021acaaa
Joel E Carlson <joel.elmer.carlson@gmail.com>
2020-11-17 15:47:43 -0600

    mkisofs.sh

--- fedora/mkisofs.sh
+++ fedora/mkisofs.sh
@@ -3,4 +3,5 @@ mkisofs -o /export/rhel8.iso \
 -c isolinux/boot.cat \
 -no-emul-boot -boot-load-size 4 -boot-info-table \
 -eltorito-alt-boot \
--e images/efiboot.img -no-emul-boot -graft-points -V "RHEL" .
+-e images/efiboot.img -no-emul-boot -graft-points \
+-V "RHEL-8-3-0-BaseOS-x86_64" .
